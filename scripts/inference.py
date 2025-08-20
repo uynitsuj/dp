3eef771698ccbfcffd738f69aa63861113f6ce1f
Justin Yu <yujustin8@gmail.com>
2025-08-19 18:00:21 -0700

    clean up inferencer

--- scripts/inference.py
+++ scripts/inference.py
@@ -160,7 +160,7 @@ def _plot_error_heatmap(
 @dataclasses.dataclass
 class InferenceConfig:
     # path to model checkpoint
-    model_ckpt_folder: str = "/home/justinyu/nfs_us/justinyu/dp/dp_xmi_rby_soup_can_intergripper_29D_20250819_170620"
+    model_ckpt_folder: str = "/home/justinyu/nfs_us/justinyu/dp/dp_xmi_rby_soup_can_intergripper_29D_20250819_174130"
     ckpt_id : int = 5
 
 def main(inference_config: InferenceConfig):
@@ -180,20 +180,10 @@ def main(inference_config: InferenceConfig):
 
     vision_transform = transforms_noaug_train(resolution=resolution) # default img_size: Union[int, Tuple[int, int]] = 224,
 
-    # import pdb; pdb.set_trace()
 
-    # train_args.dataset_cfg.action_statistics = os.path.join(args.model_ckpt_folder, 'action_statistics.json')
     train_args = replace(train_args, dataset_cfg=replace(train_args.dataset_cfg, action_statistics=os.path.join(args.model_ckpt_folder, 'action_statistics.json')), shared_cfg=replace(train_args.shared_cfg, batch_size=1))
-    # train_args.shared_cfg.batch_size = 1 
 
-    # dataset_train = SimSequenceDataset(
-    #     dataset_config=train_args.dataset_cfg,
-    #     shared_config=train_args.shared_cfg,
-    #     logging_config=train_args.logging_cfg,
-    #     vision_transform=vision_transform,
-    #     split="train",
-    #     debug=False,
-    # )
+
     if type(train_args.dataset_cfg) is DatasetConfig and train_args.dataset_cfg.dataset_root is None:
         raise ValueError("Dataset root directory is not set")
     else:
@@ -242,10 +232,8 @@ def main(inference_config: InferenceConfig):
         # # device transfer
         nbatch = {k: v.to(device) for k, v in nbatch.items()}
         pred_action = inferencer(nbatch, True)
-        # # if inferencer.model.pred_left_only:
-        # #     pred_action = pred_action[..., :10]
-        # # elif inferencer.model.pred_right_only:
-        # #     pred_action = pred_action[..., 10:]
+
+
         proprio = nbatch['proprio'].detach().to('cpu').numpy()
         T = proprio.shape[1]
         if isinstance(inferencer.model, Dinov2DiscretePolicy):
@@ -260,33 +248,8 @@ def main(inference_config: InferenceConfig):
             gt_action = torch.tensor(gt_action, device=device)
         else:
             gt_action = nbatch['action'][:,-1]
-        # # if inferencer.model.pred_left_only or inferencer.model.pred_right_only:
-        # #     gt_action = torch.concatenate([gt_action, gt_action], dim=-1)
+
         gt_action = unscale_action(gt_action, stat=inferencer.stats, type='diffusion').detach().to('cpu').numpy()
-        # # if inferencer.model.pred_left_only:
-        # #     gt_action = gt_action[..., :10]
-        # # elif inferencer.model.pred_right_only:
-        # #     gt_action = gt_action[..., 10:]
-        # #subplot of 20
-        # # if inferencer.model.pred_left_only or inferencer.model.pred_right_only:
-        # #     num_subplots = 10 
-        # #     fig, axes = plt.subplots(2, 5)
-        # # else:
-        # num_subplots = 29
-        # fig, axes = plt.subplots(6, 5)
-        # for i in range(num_subplots):
-        #     ax = axes[i//5,i%5]
-        #     ax.plot(range(T), proprio[0, :, i], label='proprio', color='green')
-        #     ax.plot(range(T, T+pred_action[0, :, i].shape[0]),pred_action[0, :, i], label='pred', color='red')
-        #     ax.plot(range(T, T+gt_action[0, :, i].shape[0]),gt_action[0, :, i], label='gt', color='blue')
-            
-        # # plt.plot(pred_action[0, :, 0], label='pred', color='red')
-        # # plt.plot(gt_action[0, :, 0], label='gt', color='blue')
-        # # plt.ylim(-1, 1)
-        # plt.legend()
-        # fig.savefig(f'pred_vs_gt_{idx}.png')
-        # if idx > 10:
-        #     break
 
         # -------------------- plotting (refined) --------------------
         # Shapes expected:
